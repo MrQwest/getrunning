f2286791c4e3ed728e69e9f7ef44f19a06281958
mrqwest <mrqwest@gmail.com>
2014-11-03 14:52:09 +0000

    and heres the converted HTML

---
+++ converted-html/assets/scripts/run.js
@@ -0,0 +1,36 @@
+$.getJSON( "run.json", function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( key + val );
+  });
+
+  itemsLength = items.length;
+  randomItem = Math.floor(Math.random() * itemsLength)
+
+ 
+  // show number of items in json array.
+  console.log("there are " + itemsLength + " items in the array");
+  console.log("The random number is " + randomItem);
+  console.log(data[randomItem].type);
+
+  if (data[randomItem].type == "quote") {
+  	$('#content').html("<p class='quote'>" + data[randomItem].resource + "</p>");
+  } else if (data[randomItem].type == "video") {
+  	// if youtube video, strip out unneeded sections and feed into embed code.
+  	var video_id = data[randomItem].resource.split('v=')[1];
+	var ampersandPosition = video_id.indexOf('&');
+	if(ampersandPosition != -1) {
+	  video_id = video_id.substring(0, ampersandPosition);
+	}
+	console.log(video_id);
+	$('#content').append("<div class='videoWrapper'><iframe width=560 height=315 src='//www.youtube.com/embed/" + video_id + "' frameborder='0' allowfullscreen></iframe></div>");
+
+	// $('#content').html("<p>" + data[randomItem].resource + "</p>");
+  } else if (data[randomItem].type == "image") {
+  	$('#content').html("<p>" + data[randomItem].resource + "</p>");
+  } else {
+  	$('#content').html("<p>Just Run!</p>");
+  };
+
+  $('#content').append(items[randomItem].type);
+});
